75aa9004188bc3e7f6054971319657e4e3384543
Raz <razgavrieli@gmail.com>
2023-05-17 20:06:35 +0300

    to spectorgram ready

--- data_process/wav_to_spectogram.py
+++ data_process/wav_to_spectogram.py
@@ -9,54 +9,61 @@ import os
 
 import torch
 import torchaudio
+import sys
 
+if __name__ == "__main__":
+  arg1 = sys.argv[1]
+  if arg1 not in ['russian', 'arabic', 'english', 'french']:
+    print("bad language")
+    exit(1)
+  language_path = "data/"+arg1+"_data"
+  temp_path = "temp"
+  output_path = "data/json_"+arg1+"_data"
+  if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
-russian_path = "data/russian_data"
-temp_path = "temp"
-output_path = "data/spectogram_russian_data"
+  torch.random.manual_seed(0)
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  print("device", device)
+  bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+  model = bundle.get_model().to(device)
 
-torch.random.manual_seed(0)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("device", device)
-bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
-model = bundle.get_model().to(device)
+  i = 0
 
-i = 0
+  female_folder = glob.glob(os.path.join(language_path, "female_"+arg1))[0]
+  wav_files = glob.glob(os.path.join(female_folder, '*.mp3'))
+  for wav_file in wav_files:
+    i += 1
+    # if i == 5: # REMOVE TO PROCESS ALL DATA
+      # break
+    print("proccessing..", wav_file, i)
+    sound = AudioSegment.from_mp3(wav_file)
+    sound.export(temp_path+"/temp_wav_to_spectogram.wav", format="wav")
 
-female_folder = glob.glob(os.path.join(russian_path, 'female_russian'))[0]
-wav_files = glob.glob(os.path.join(female_folder, '*.mp3'))
-for wav_file in wav_files:
-  i += 1
-  # if i == 5: # REMOVE TO PROCESS ALL DATA
-    # break
-  print("proccessing..", wav_file, i)
-  sound = AudioSegment.from_mp3(wav_file)
-  sound.export(temp_path+"/temp_wav_to_spectogram.wav", format="wav")
+    sample_rate, samples = wavfile.read(temp_path+"/temp_wav_to_spectogram.wav")
+    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
 
-  sample_rate, samples = wavfile.read(temp_path+"/temp_wav_to_spectogram.wav")
-  frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+    plt.ylim([0, 4000])  # set y-axis limits to show only relevant frequencies
+    plt.pcolormesh(times, frequencies, spectrogram)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.savefig(output_path+"/W/"+str(i)+'.png')
 
-  plt.ylim([0, 4000])  # set y-axis limits to show only relevant frequencies
-  plt.pcolormesh(times, frequencies, spectrogram)
-  plt.ylabel('Frequency [Hz]')
-  plt.xlabel('Time [sec]')
-  plt.savefig(output_path+"/W/"+str(i)+'.png')
+  male_folder = glob.glob(os.path.join(language_path, "male_"+arg1))[0]
+  wav_files = glob.glob(os.path.join(male_folder, '*.mp3'))
+  for wav_file in wav_files:
+    i += 1
+    # if i == 10:  # REMOVE TO PROCESS ALL DATA
+      # break
+    print("proccessing..", wav_file, i)
+    sound = AudioSegment.from_mp3(wav_file)
+    sound.export(temp_path+"/temp_wav_to_spectogram.wav", format="wav")
 
-male_folder = glob.glob(os.path.join(russian_path, 'male_russian'))[0]
-wav_files = glob.glob(os.path.join(male_folder, '*.mp3'))
-for wav_file in wav_files:
-  i += 1
-  # if i == 10:  # REMOVE TO PROCESS ALL DATA
-    # break
-  print("proccessing..", wav_file, i)
-  sound = AudioSegment.from_mp3(wav_file)
-  sound.export(temp_path+"/temp_wav_to_spectogram.wav", format="wav")
+    sample_rate, samples = wavfile.read(temp_path+"/temp_wav_to_spectogram.wav")
+    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
 
-  sample_rate, samples = wavfile.read(temp_path+"/temp_wav_to_spectogram.wav")
-  frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-
-  plt.ylim([0, 4000])  # set y-axis limits to show only relevant frequencies
-  plt.pcolormesh(times, frequencies, spectrogram)
-  plt.ylabel('Frequency [Hz]')
-  plt.xlabel('Time [sec]')
-  plt.savefig(output_path+"/M/"+str(i)+'.png')
+    plt.ylim([0, 4000])  # set y-axis limits to show only relevant frequencies
+    plt.pcolormesh(times, frequencies, spectrogram)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.savefig(output_path+"/M/"+str(i)+'.png')
